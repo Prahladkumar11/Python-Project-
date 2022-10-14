3065786ffcdcc8585801805b372f1bf33b7e6cd2
Prahlad kumar kushwaha <Prahladkuma11@gmail.com>
2022-10-14 12:09:22 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,44 +1,60 @@
-print('======= CALCULATOR=========')
-c=int(input('Enter the no  for operation a:'))
-d=int(input('Enter the no for operation b:'))
+def calculator():
+    print('======= CALCULATOR=========')
+    c = int(input('Enter the no  for operation a:'))
+    d = int(input('Enter the no for operation b:'))
 
-print('\n-----CHOOSE THE OPERATION TO PERFORM-----')
-def sum(a,b):
-    a+=b
-    return a
-def sub(a,b):
-    a-=b
-    return a
-def mul(a,b):
-    a*=b
-    return a
-def div(a,b):
-    a//=b
+    print('\n-----CHOOSE THE OPERATION TO PERFORM-----')
+    cal(c,d)
+    
+    calculator()
+
+
+def sum(a, b):
+    a += b
     return a
 
 
-print('1:Additon')
-print('2:Subtration')
-print('3:Multiplication')
-print('4:Division')
-print('5:exti')
+def sub(a, b):
+    a -= b
+    return a
 
-choice= int(input('>>>'))
 
-if choice == 1:
-    s= sum(c,d)
-    print('The sum of the no {} and {} is {}'.format(c,d,s))
+def mul(a, b):
+    a *= b
+    return a
 
-elif choice ==2:
-    s=sub(c,d)
-    print('The sub of the no {} and {} is {}'.format(c,d,s))
-elif choice ==3:
-    s=mul(c,d)
-    print('The mul of the no {} and {} is {}'.format(c,d,s))
 
-elif choice ==4:
-    s=div(c,d)
-    print('The div of the no {} and {} is {}'.format(c,d,s))
+def div(a, b):
+    a //= b
+    return a
 
-else :
-    print('----Thanks you-------')
+def cal(c,d):
+    print('1:Additon')
+    print('2:Subtration')
+    print('3:Multiplication')
+    print('4:Division')
+    print('5:exti')
+
+    choice = int(input('>>>'))
+
+    if choice == 1:
+        s = sum(c, d)
+        print('The sum of the no {} and {} is {}'.format(c, d, s))
+
+    elif choice == 2:
+        s = sub(c, d)
+        print('The sub of the no {} and {} is {}'.format(c, d, s))
+    elif choice == 3:
+        s = mul(c, d)
+        print('The mul of the no {} and {} is {}'.format(c, d, s))
+
+    elif choice == 4:
+        s = div(c, d)
+        print('The div of the no {} and {} is {}'.format(c, d, s))
+
+    elif choice==5:
+        print('----Thanks you-------')
+        return 0
+
+
+calculator()
